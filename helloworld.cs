a7ea6d07e770b51a50552c7da14c341e83dd20c6
Nishant Karki <nishantkarki@Nishants-Laptop.local>
2023-03-27 16:51:42 -0500

    added some info

--- helloworld.cs
+++ helloworld.cs
@@ -47,8 +47,17 @@ namespace HelloWorldApplication{
 	}// Close of Rectangle
 	
 	
+	struct Books
+	{
+	public string title;
+    public string author;
+    public string subject;
+    public int book_id;
+	};
+
 	class MainClass{
-		
+		enum Months {JAN, FEB, MAR, APR}; //This enum should be outside the Main fxn
+		// but can be inside the class. s
 		static void Main(String[] args){
 		//This Main method can be within the Rectangle class as well but it has to be within a class. Without
 		// class it can not exists. 
@@ -67,13 +76,80 @@ namespace HelloWorldApplication{
 		obj.Display();
 
 		Console.WriteLine("size of obj={0}", sizeof(int));
-		Console.WriteLine("size of obj={0}", sizeof(r));
-		Console.ReadKey();
+		
+
+
+
+	/*
+	C# nullable types to which we can assign normal range calues as well as null 
+	values. 
+    For example, you can store any value from -2,147,483,648 to 2,147,483,647 or null in a Nullable<Int32> variable. 
+	Similarly, you can assign true, false, or null in a Nullable<bool>
+	syntax:
+	<datatype> ? <variablename>= null	
+	*/
+
+	int? num1 = null;
+
+	int? num2 = 45;
+	double? num3 = new double?();
+
+	double? num4 = 3.14;
+
+	bool? boolVal = new bool?();
+
+	Console.WriteLine(@"nullable values num1 = {0}, num2 = {1} 
+	num3 = {2}, num4 = {3}, boolVal = {4}",num1, num2, num3, num4, boolVal);
+
+
+	// arrays 
+	double[] balance = { 2340.0, 4523.69, 3421.0};
+	// structs can be declared like function calls
+	// structs hold the similar record together
+	Books book1 = new Books();
+	Books book2;
+
+	book1.title = "Learning C#";
+	book1.book_id = 12;
+	book1.author = "BHOJ";
+	book1.subject = "programming with c#";
+
+
+	book2.title = "Data Structure and Algorithms";
+	book2.book_id = 2;
+	book2.author = "BHOJ";
+	book2.subject = "programming with python";
+
+	Console.WriteLine("book 1 title ={0}, book2 auth={1} ", book1.title, book2.author);
+
+	
+
+	/*
+	Enums => named integer constants.
+
+	An enumeration is a set of named integer constants. An enumerated type is declared using the enum keyword.
+
+	C# enumerations are value data type. In other words, enumeration contains its own values and cannot inherit or cannot pass inheritance.
+	
+	enum <enumname> {enumeration_values};
+	*/
+
+	
+
+	int monthStart = (int)Months.JAN;
+	int monthEnd = (int)Months.APR; //in enum only upto apirl
+
+	Console.WriteLine("Start {0} End  {1}", monthStart, monthEnd);
+
+
+
+
+	Console.ReadKey();
 		
 		
 	
-		}	
-	} // main done
+	}	// main done
+	} // MainClass close
 	
 	
 	//}
